5187f64d67b72829cbe9b34528a6e3787d844ced
mikedarling12 <mdarl1ng@byui.edu>
2023-11-08 17:11:45 -0800

    week 09 group project part 2

--- assignment1/scripts/links.js
+++ assignment1/scripts/links.js
@@ -5,6 +5,28 @@ async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
     console.log(data);
+    displayLinks(data);
+}
+
+function displayLinks(data) {
+    const lessons = data.lessons;
+    let activitiesList = document.getElementById("activitiesList");
+    lessons.forEach(lesson => {
+        let week = document.createElement('li');
+        week.textContent = `Week ${lesson.lesson}:`;
+
+        lesson.links.forEach(assignment => {
+            let link = document.createElement('a');
+
+            let linkText = document.createTextNode(assignment.title);
+            link.appendChild(linkText);
+            //link.textContent = assignment.title;
+            link.href = assignment.url;
+            //week.textContent += " " + link;
+            week.appendChild(link);
+        });
+        activitiesList.appendChild(week);
+    });
 }
 
 getLinks();
