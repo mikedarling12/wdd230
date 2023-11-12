455450fc91411accacf308f7ef61609fe61131c1
mikedarling12 <mdarl1ng@byui.edu>
2023-11-12 00:14:32 -0800

    weekend assignment part 2

--- chamber/scripts/companies.js
+++ chamber/scripts/companies.js
@@ -14,6 +14,7 @@ function displayCompanies(data) {
         const companyAddress = document.createElement('p');
         const companyPhone = document.createElement('p');
         const companyWebsite = document.createElement('p');
+        const membershipLevel = document.createElement('p');
 
         const portrait = document.createElement('img');
 
@@ -24,17 +25,34 @@ function displayCompanies(data) {
         companyAddress.textContent = `${company.address}`;
         companyPhone.textContent = `${company.phoneNumber}`;
         companyWebsite.textContent = `${company.websiteURL}`;
+        membershipLevel.textContent = `Membership Level: ${company.membership}`;
         
         card.appendChild(portrait);
         card.appendChild(companyName);
         card.appendChild(companyAddress);
         card.appendChild(companyPhone);
         card.appendChild(companyWebsite);
+        card.appendChild(membershipLevel);
 
         card.setAttribute("id","company");
         
         cards.appendChild(card);
     });
 }
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("#cards");
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", () => {
+	display.classList.add("list");
+	display.classList.remove("grid");
+});
 
 getCompanyData();
